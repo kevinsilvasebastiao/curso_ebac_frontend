d86662a9d4c9ec0753c74a0085efd8f73b0252e8
kevin <kevinsilva431@gmail.com>
2023-07-25 20:14:48 -0300

    exercicio

---
+++ main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    // Máscara para CPF: 000.000.000-00
+    $('#cpf').mask('000.000.000-00');
+    
+    // Máscara para CEP: 00000-000
+    $('#cep').mask('00000-000');
+});
