b19bf45b842023063a0cee227856289a7aa6e1f5
kevin <kevinsilva41@gmail.com>
2024-04-26 18:41:11 -0300

    Adicionando código TypeScript para o exercício

---
+++ exercicio_ts/src/exercicio.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicacao(num1, num2) {
+    return num1 * num2;
+}
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+console.log(multiplicacao(2, 3));
+console.log(saudacao("João"));
---
+++ exercicio_ts/src/exercicio.ts
@@ -0,0 +1,10 @@
+function multiplicacao(num1: number, num2: number): number {
+    return num1 * num2;
+}
+
+function saudacao(nome: string): string {
+    return "Olá " + nome;
+}
+
+console.log(multiplicacao(2, 3));
+console.log(saudacao("João"));
